32e0fd5cc49ad0a4d1418672e60ec54fe327e3c0
Patryk <ssnickers700@gmail.com>
2022-11-11 12:09:31 +0100

    Add js client form validation

---
+++ js/validationClientForm.js
@@ -0,0 +1,58 @@
+function validateForm() {
+    const firstNameInput = document.getElementById("firstName");
+    const lastNameInput = document.getElementById("lastName");
+    const emailInput = document.getElementById("email");
+
+    const errorFirstName = document.getElementById("errorFirstName");
+    const errorLastName = document.getElementById("errorLastName");
+    const errorEmail = document.getElementById("errorEmail");
+    const errorsSummary = document.getElementById("errorsSummary");
+
+    resetErrors(
+        [firstNameInput, lastNameInput, emailInput],
+        [errorFirstName, errorLastName, errorEmail, errorsSummary],
+        errorsSummary
+    );
+
+    let valid = true;
+
+    if (!checkRequired(firstNameInput.value)) {
+        valid = false;
+        firstNameInput.classList.add("error-input");
+        errorFirstName.innerText = "Pole jest wymagane";
+    } else if (!checkTextLengthRange(firstNameInput.value, 2, 60)) {
+        valid = false;
+        firstNameInput.classList.add("error-input");
+        errorFirstName.innerText = "Pole powinno zawierać od 2 do 60 znaków";
+    }
+
+    if (!checkRequired(lastNameInput.value)) {
+        valid = false;
+        lastNameInput.classList.add("error-input");
+        errorLastName.innerText = "Pole jest wymagane";
+    } else if (!checkTextLengthRange(lastNameInput.value, 2, 60)) {
+        valid = false;
+        lastNameInput.classList.add("error-input");
+        errorLastName.innerText = "Pole powinno zawierać od 2 do 60 znaków";
+    }
+
+    if (!checkRequired(emailInput.value)) {
+        valid = false;
+        emailInput.classList.add("error-input");
+        errorEmail.innerText = "Pole jest wymagane";
+    } else if (!checkTextLengthRange(emailInput.value, 5, 60)) {
+        valid = false;
+        emailInput.classList.add("error-input");
+        errorEmail.innerText = "Pole powinno zawierać od 5 do 60 znaków";
+    } else if (!checkEmail(emailInput.value)) {
+        valid = false;
+        emailInput.classList.add("error-input");
+        errorEmail.innerText = "Pole powinno zawierać prawidłowy adres email";
+    }
+
+    if (!valid) {
+        errorsSummary.innerText = "Formularz zawiera błędy";
+    }
+
+    return valid;
+}
---
+++ js/validationCommon.js
@@ -0,0 +1,35 @@
+function resetErrors(inputs, errorTexts, errorInfo) {
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].classList.remove("error-input");
+    }
+    for (let i = 0; i < errorTexts.length; i++) {
+        errorTexts[i].innerText = "";
+    }
+    errorInfo.innerText = "";
+}
+
+function checkRequired(value) {
+    if (!value) {
+        return false;
+    }
+    value = value.toString().trim();
+    return value !== "";
+}
+
+function checkTextLengthRange(value, min, max) {
+    if (!value) {
+        return false;
+    }
+    value = value.toString().trim();
+    const length = value.length;
+    return !((max && length > max) || (min && length < min));
+}
+
+function checkEmail(value) {
+    if (!value) {
+        return false;
+    }
+    value = value.toString().trim();
+    const re = /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    return re.test(value);
+}
