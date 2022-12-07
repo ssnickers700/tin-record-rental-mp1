dbea1c33bf8767da9a5e95cdddba305ca34e27ea
Patryk <ssnickers700@gmail.com>
2022-12-07 21:50:46 +0100

    Add record repository

---
+++ repository/sequelize/RecordRepository.js
@@ -0,0 +1,41 @@
+const Client = require("../../model/sequelize/Client");
+const Rental = require("../../model/sequelize/Rental");
+const Record = require("../../model/sequelize/Record");
+
+exports.getRecords = () => {
+    return Record.findAll();
+}
+
+exports.getRecordById = (recordId) => {
+    return Record.findByPk(recordId,
+        {
+            include: [{
+                model: Rental,
+                as: "rentals",
+                include: [{
+                    model: Client,
+                    as: "client"
+                }]
+            }]
+        });
+}
+
+exports.createRecord = (newRecordData) => {
+    return Record.create({
+        recordName: newRecordData.recordName,
+        artistName: newRecordData.artistName,
+        price: newRecordData.price,
+        unit: newRecordData.unit
+    });
+}
+
+exports.updateRecord = (recordId, recordData) => {
+    return Record.update(recordData, {where: {_id: recordId}});
+}
+
+exports.deleteRecord = (recordId) => {
+    return Record.destroy({
+        where: {_id: recordId}
+    })
+}
+
